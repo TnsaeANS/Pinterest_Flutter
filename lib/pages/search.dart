4c8a812d3e7e13e1d54fcead8f69c41e1fea9dc6
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2025-01-11 16:15:53 +0300

    assets for search

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -7,41 +9,61 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
+    List<String> imagePaths = [
+      'assets/22.jpg',
+      'assets/23.jpg',
+      'assets/24.jpg',
+      'assets/25.jpg',
+      'assets/26.jpg',
+      'assets/27.jpg',
+      'assets/28.jpg'
+    ];
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
             width: screenWidth,
-            height: 60,
+            height: 50,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: TextFormField(
+                style: TextStyle(fontSize: 17, color: Colors.black),
                 decoration: InputDecoration(
                   hintText: 'Search for ideas',
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: Icon(Icons.camera_alt),
-
+                  prefixIcon: Icon(Icons.search, color: Colors.grey.shade400),
+                  suffixIcon: Icon(Icons.camera_alt, color: Colors.grey.shade400),
+                  hintStyle: TextStyle(fontSize: 18, color: Colors.grey.shade500),
                   border: OutlineInputBorder(
-                  
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(40), 
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(40),
                   ),
                   filled: true,
-                  fillColor: Colors.grey.shade200,
+                  fillColor: const Color.fromARGB(117, 223, 223, 223),
                 ),
               ),
             ),
           ),
-          const SizedBox(height: 25),
+          const SizedBox(height: 30),
           const Text(
-          "Ideas for you",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-                    ),
-                    )],
+            "Ideas for you",
+            style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          Expanded(
+            child: MasonryGridView.builder(
+              gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: imagePaths.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                  child: Image.asset(imagePaths[index], fit: BoxFit.cover),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
